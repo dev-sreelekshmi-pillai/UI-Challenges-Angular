ca9017a378859ce71bad18fc1cc3293825ae6e09
dev-sreelekshmi-pillai <sree0008lakshmi@gmail.com>
2025-03-01 21:23:15 +0530

    Angular challenged UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Challenge01Component } from './challenges/challenge01/challenge01.component';
+import { Challenge02Component } from './challenges/challenge02/challenge02.component';
+import { Challenge03Component } from './challenges/challenge03/challenge03.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'challenge01', component: Challenge01Component },
+    { path: 'challenge02', component: Challenge02Component },
+    { path: 'challenge03', component: Challenge03Component },
+    { path: '', redirectTo: 'challenge01', pathMatch: 'full' }, // Default route
+
+];
---
+++ src/app/challenges/challenge02/challenge02.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge02',
+  standalone: true,
+  imports: [],
+  templateUrl: './challenge02.component.html',
+  styleUrl: './challenge02.component.css'
+})
+export class Challenge02Component {
+
+}
