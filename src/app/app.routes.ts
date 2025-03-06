18910d376a1fb3ab68f57d36100f33fde068f4d6
dev-sreelekshmi-pillai <sree0008lakshmi@gmail.com>
2025-03-06 12:55:37 +0530

    Added home page and updated concert UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Challenge01Component } from './challenges/challenge01/challenge01.component';
 import { Challenge02Component } from './challenges/challenge02/challenge02.component';
 import { Challenge03Component } from './challenges/challenge03/challenge03.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: 'challenge01', component: Challenge01Component },
     { path: 'challenge02', component: Challenge02Component },
     { path: 'challenge03', component: Challenge03Component },
-    { path: '', redirectTo: 'challenge01', pathMatch: 'full' }, // Default route
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, // Default route
 
 ];
